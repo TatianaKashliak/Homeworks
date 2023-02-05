28627fd6810c45cab857fa21e6319e5761d021ea
TatianaKashliak <tatianakashliak@gmail.com>
2023-02-06 00:02:22 +0300

    initial commit

---
+++ GitProject/src/by/TeachMeSkills/Homeworks/hm_10022023/FirstHomeworkClass.java
@@ -0,0 +1,10 @@
+package by.TeachMeSkills.Homeworks.hm_10022023;
+
+public class FirstHomeworkClass {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 9;
+        int sum = a+b;
+        System.out.println("Sum: " +sum);
+    }
+}
